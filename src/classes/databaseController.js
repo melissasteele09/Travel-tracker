e55d754e065bcb4a2f212f8faf61a99a7caaa65e
Melissa Steele <melissa.steele09@gmail.com>
2020-03-01 13:17:10 -0700

    Load User

--- src/classes/databaseController.js
+++ src/classes/databaseController.js
@@ -2,6 +2,10 @@ import Agency from './classes/agency.js'
 import domUpdates from './domUpdates.js'
 import Traveler from './classes/traveler.js'
 
+let allUsers;
+let allTrips;
+let allDestinations;
+let traveler;
 
 const userData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
   .then(response => response.json())
@@ -17,6 +21,24 @@ const destinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-trac
   .then(response => response.json())
   .then(data => data.destinations)
   .catch(error => console.log(error.message))
+
+
+Promise.all([userData, tripsData, destinationsData])
+  .then(data => {
+    allUsers = data[0]
+    allTrips = data[1]
+    allDestinations = data[2]
+  })
+
+
+const databaseController = {
+
+  loadUser(id) {
+    let user = allUsers[id - 1]
+    let trips = allTrips.filter(trip => trip.userID === parseInt(id))
+    traveler = new Traveler(user, trips)
+    domUpdates.loadTraveler(traveler, allDestinations)
+  },
 
 
 export default databaseController;
