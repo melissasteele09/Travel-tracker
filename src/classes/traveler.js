19b636e2d645e0ab6bfd2a2b3087a22f5b00d5ec
Melissa Steele <melissa.steele09@gmail.com>
2020-03-01 15:58:45 -0700

    Add properties to traveler class

---
+++ src/classes/traveler.js
@@ -0,0 +1,16 @@
+import UserUtility from './userUtilities.js';
+
+
+class Traveler extends Utility {
+  constructor(info, trips) {
+    super()
+    this.id = info.id,
+    this.name = info.name,
+    this.travelerType = info.travelerType,
+    this.trips = trips
+  }
+
+}
+
+
+export default Traveler
