c9e2d2c969b18f009653c68573db553fb611a9ef
Melissa Steele <melissa.steele09@gmail.com>
2020-03-01 13:15:06 -0700

    Fetch data

---
+++ src/classes/databaseController.js
@@ -0,0 +1,22 @@
+import Agency from './classes/agency.js'
+import domUpdates from './domUpdates.js'
+import Traveler from './classes/traveler.js'
+
+
+const userData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
+  .then(response => response.json())
+  .then(data => data.travelers)
+  .catch(error => console.log(error.message))
+
+const tripsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips')
+  .then(response => response.json())
+  .then(data => data.trips)
+  .catch(error => console.log(error.message))
+
+const destinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/destinations/destinations')
+  .then(response => response.json())
+  .then(data => data.destinations)
+  .catch(error => console.log(error.message))
+
+
+export default databaseController;
